da20b68ae059f903cc59cb2af4b2a4cdda22e5b5
Ott Ottender <Ott.Ottender@gmail.com>
2024-12-20 03:56:15 +0200

    first commit

--- server.js
+++ server.js
@@ -1,18 +1,23 @@
-
 const express = require('express');
 const bodyParser = require('body-parser');
-const todoRouter = require('./routes/todo.routes');
+const todoRouter = require('./routes/todo.routes');  
 const dotenv = require('dotenv');
 const app = express();
 
+
 dotenv.config();
 
+app.use(bodyParser.json()); 
 
-app.use(bodyParser.json());  
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
+app.use('/tasks', todoRouter);
 
-app.use('/todo', todoRouter);
-
-app.listen(3000, () => {
-    console.log('Server is running on Port 3000.');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on Port ${PORT}.`);
 });
